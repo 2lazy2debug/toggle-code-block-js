eaebb25eb2d6304b8ed685676fab5b205634af28
2lazy2debug <cabrasm@outlook.com>
2021-01-22 00:32:16 +0100

    remove useless comments

--- extension.js
+++ extension.js
@@ -1,38 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	console.log('Congratulations, your extension "Toggle Code Block" is now active!');
-
 	let disposable = vscode.commands.registerCommand('simple-comment-toggle.toggleCodeBlock', function () {
-
 		const editor = vscode.window.activeTextEditor;
+
 		var position;
 
 		if (editor.selection.isEmpty) {
-			console.log('got here')
 			position = editor.selection.active;
 			//Find all comment blocks
 			editor.document.getText().match(/(\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+\/)|(\/\/.*)/g).forEach(
 				commentBlock => {
-					console.log('and here')
-
 					//comment block array
 					let cbArray = commentBlock.split('\n');
 
-					vscode.window.showInformationMessage(`cursor position line: ${position.line}`);
-
 					let comIndex = cbArray.findIndex(
-						(commentLine, index) => {
-							console.log('also here with ' + commentLine + ' feat ' + index)
+						(commentLine) => {
 							return commentLine.includes(editor.document.lineAt(position.line).text) 
 						}
 					);
@@ -43,7 +30,6 @@ function activate(context) {
 
 						editor.edit(editBuilder => {
 							let range = new vscode.Range(comStartLine, 0,comEndLine, (cbArray[cbArray.length-1].length))
-							console.log(range)
 							editBuilder.replace(range, commentBlock.replace(new RegExp(/\/\**|\**\//g), "").replace("*", ""))
 						})
 					}
@@ -51,9 +37,6 @@ function activate(context) {
 				}
 			);
 		}
-
-		// Display a message box to the user
-		//vscode.window.showInformationMessage(`cursor position line: ${position.line}, cursor position char: ${position.character}`);
 	});
 
 	context.subscriptions.push(disposable);
